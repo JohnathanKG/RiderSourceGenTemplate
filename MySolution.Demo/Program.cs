1ce72efce3864fd7cf75f9987f3aff6cd87d0750
John <airmasterdude@gmail.com>
2023-05-21 23:18:36 -0500

    Add template code

---
+++ MySolution.Demo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MySolution.Demo;
+
+static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(HelloWorld.Message);
+    }
+}
---
+++ MySolution.Gen/HelloWorldGen.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace MySolution.Gen;
+
+[Generator]
+public class HelloWorldGen : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(PostInitializationCallback);
+            
+    }
+
+    private static void PostInitializationCallback(IncrementalGeneratorPostInitializationContext context)
+    {
+        context.AddSource($"{nameof(MySolution)}.g.cs", $@"public class HelloWorld {{ static public string Message => ""Hello World!""; }}");
+    }
+}
